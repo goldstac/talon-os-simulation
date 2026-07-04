e70204fbfdbc7981bf7f4d250e1dcda0a074a879
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-04 21:11:45 +0400

    big update

--- system/free-h.cpp
+++ system/free-h.cpp
@@ -1,5 +1,6 @@
+#include <free_h.h>
 #include <iostream>
-int main() {
+void show_memory() {
   std::cout << "Memory: Free : Using\n";
   std::cout << "96 GB : 93GB : 3 GB  ";
   return 0;
---
+++ system/free_h.h
@@ -0,0 +1,4 @@
+#ifndef free_h
+#define free_h
+void show_memory();
+#endif
