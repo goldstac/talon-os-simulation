93ac85f1d04a2d97f997db1980c1f1e091e8a8bd
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-04 19:44:28 +0400

    update kernel

---
+++ kernel/compat.h
@@ -0,0 +1,12 @@
+#ifndef COMPAT_H
+#define COMPAT_H
+
+#ifdef _WIN32
+#include <windows.h>
+#define sleep_ms(ms) Sleep(ms)
+#else
+#include <unistd.h>
+#define sleep_ms(ms) usleep((ms) * 1000)
+#endif
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,10 @@
+#include "compat.h"
 #include <stdio.h>
 int main() {
-  printf("hello im a kernel");
+  printf("Starting Linux\n");
+  sleep_ms(2000);
+  printf("starting ramdisk\n");
+  sleep_ms(2000);
+  printf("/dev/nvme0n1p\n");
   return 0;
 }
