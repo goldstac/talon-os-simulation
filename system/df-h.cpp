11ed69c5d30c220d944c2cc45941b515b9554da8
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-06 03:48:43 +0400

    Update df-h.cpp

--- system/df-h.cpp
+++ system/df-h.cpp
@@ -2,6 +2,6 @@
 void disk_free(){
     std::cout << "Filesystem |     Path     | Size | Used | Avail |\n";
     std::cout << "-----------|--------------|--------\n";
-    std::cout << "ext4       |/dev/nme0n1p6 |";
+    std::cout << "ext4       |/dev/nme0n1p6 |      |";
     std::cout << "esp        |              |    \n";
 }
