851fcd84665714d0aef823e8695c42a5d7160d03
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-05 18:28:24 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# runs make file
+echo "Running MAKE"
 MAKE
+# compiles main and kernel
+echo "Compiling Main & Kernel"
 g++ -o main main.cpp kernel/kernel.c
+# run main
+echo "Running Main"
 ./main
