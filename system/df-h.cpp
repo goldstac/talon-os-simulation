7d7335fc79df1cf84a4752c81ae5f2f1c62a7c46
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-06 03:28:51 +0400

    Update df-h.cpp

--- system/df-h.cpp
+++ system/df-h.cpp
@@ -2,5 +2,5 @@
 void disk_free(){
     std::cout << "Filesystem   : Size : Used : Avail\n";
     std::cout << "ext4         : 67TB :      :        ";
-    std::cout << "esp          :  1G  :      :        ";
+    std::cout << "esp          :  1G  : 222M :        ";
 }
