110d46677d726e422587b2f8387a62fe4299fb9c
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-06 03:44:59 +0400

    Update df-h.cpp

--- system/df-h.cpp
+++ system/df-h.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 void disk_free(){
-    std::cout << "Filesystem   | Size |  3TB |  64TB  \n";
+    std::cout << "Filesystem  Path | Size |  3TB |  64TB  \n";
     std::cout << "-------------|------|------|--------\n";
     std::cout << "esp          |  1G  | 222M | 778M   \n";
 }
