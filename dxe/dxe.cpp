b07cdff8c3ce313c05c01ffa57bdf85b02358469
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-05 17:40:15 +0400

    big dxe driver update

--- dxe/dxe.cpp
+++ dxe/dxe.cpp
@@ -1,4 +1,58 @@
 #include <iostream>
-int dxe() {
-    
+int drivers_init(){
+
+}
+int storage_driver(){
+drivers_init();
+}
+int displayport_driver(){
+drivers_init();
+}
+int hdmi_driver(){
+drivers_init();
+}
+int ram_driver(){
+drivers_init();
+}
+int pcie_bus_driver(){
+drivers_init();
+}
+int efi_driver(){
+drivers_init();
+}
+int usb_driver(){
+drivers_init();
+}
+int ethernet_driver(){
+drivers_init();
+}
+int wifi_driver(){
+drivers_init();
+}
+int cpu_driver(){
+drivers_init();
+}
+int gpu_driver(){
+drivers_init();
+}
+int nvme_driver(){
+drivers_init();
+}
+int sata_driver(){
+drivers_init();
+}
+int ahci_driver(){
+drivers_init();
+}
+int keyboard_driver(){
+drivers_init();
+}
+int mouse_driver(){
+drivers_init();
+}
+int audio_driver(){
+drivers_init();
+}
+int ide_driver(){
+drivers_init();
 }
