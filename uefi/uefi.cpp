641ed5c50585f387bd471868cfdb8e2714487d79
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-06 22:09:58 +0400

    Update uefi.cpp

--- uefi/uefi.cpp
+++ uefi/uefi.cpp
@@ -9,13 +9,15 @@ std::string uefi_user_input;
     cout << "Welcome TO Unified Extensible Firmware Interface";
 cout << "...";
 cout << " Unified Extensible Firmware Interface";
-cout << "---------------------------------------";
-cout << "| 1|Boot Order | Others      |          |";
-cout << "| 2|           | Secure Boot |          |";
-cout << "| 3|           |             |          |";
-cout << "| 4|           |             |          |"; 
+cout << "-----------------------------------------";
+cout << "|Boot Order    |  Other                 |";
+cout<< "----------------|------------------------|";
+cout << "| 1| Talon     | Secure Boot | Disabled |";
+cout << "| 2| Arch      |   XMP       | Enabled  |";
+cout << "| 3| USB       |   TPM       | Enabled  |"; 
 cout << "|---------------------------------------|";
 cout << "| NVRAM IS LOCATED AT ~/nvram/nvram.cpp |";
 cout << "|---------------------------------------|";
+
 
 }
