8c809cfad9cb7da19a4109e1ae7164f45ec7553f
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-06 21:59:51 +0400

    big uefi update 👍

--- uefi/uefi.cpp
+++ uefi/uefi.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <string>
+#include "uefi.h"
+using namespace std;
+void uefi(){
+std::string uefi_user_input;
+    bool boot_priority_talon = false;
+    cout << "Booting To UEFI\n";
+    cout << "Welcome TO Unified Extensible Firmware Interface";
+cout << "...";
+cout << " Unified Extensible Firmware Interface";
+cout << "---------------------------------------";
+cout << "| 1|Boot Order | Others      |          |";
+cout << "| 2|           | Secure Boot |          |";
+cout << "| 3|           |             |          |";
+cout << "| 4|           |             |          |"; 
+cout << "|---------------------------------------|";
+cout << "| NVRAM IS LOCATED AT ~/nvram/nvram.cpp |";
+cout << "|---------------------------------------|";
+
+}
