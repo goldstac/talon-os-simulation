3cd176398497cb490991b2800355548cedf4772d
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-10 19:53:53 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,8 +29,18 @@ int main() {
      std::cout << "";
     }
     else if (shell == "free -h") {
-     show_memory();
+     std::cout << "Memory: Free : Using\n";
+     std::
+     std::cout << "96 GB : 93GB : 3 GB  \n";
      continue;
+    }
+    else if (shell == "df -h"){
+    std::cout << "Filesystem |     Path     | Size | Used | Avail \n";
+    std::cout << "-----------|--------------|------|------|-------\n";
+    std::cout << "ext4       |/dev/nvme0n1p6| 8TB  | 1TB  |  6TB  \n";
+    std::cout << "ext4       |/dev/nvme0n1p7| 8TB  | 200G | 7.8TB \n";
+    std::cout << "esp        |/dev/nvmeon1p5|  1G  | 221M |  779M \n";
+    continue;
     }
     else{
      std::cout << "Command Not Found\n";
