c193b59ef6815d1f62e18c3e937689d27012a28b
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-10 20:31:58 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,6 +35,7 @@ int main() {
      std::cout << "Storage Driver\n";
      std::cout << "Storage Driver\n";
      std::cout << "Storage Driver\n";
+     std::cout << "Storage Driver\n";
      //bruh
 
     }
