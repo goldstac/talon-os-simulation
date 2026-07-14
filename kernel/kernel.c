ab2253f6a54a35faaf1e9ecd1aa65aa8a48a5189
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-14 21:42:40 +0400

    big

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,17 @@
 #include "kernel.h"
 #include "compat.h"
 #include <stdio.h>
-void driver_log(char log[50]) {
-
-}
-void kernel(){
+void driver_log(char log[50]) {}
+void kernel() {
   printf("Starting Linux linux ...\n");
   sleep_ms(2000);
   driver_log("[DRIVER] UEFI DRIVER LOADING");
   driver_log("[DRIVER] HDMI DRIVER LOADING");
   driver_log("[DRIVER] SEC DRIVER LOOADING");
   driver_log("[DRIVER] PEI DRIVER LOADING");
-  driver_log("[DRIVER] RAM DRIVER LOADING");
+  driver_log("[DRIVER] RAM DRIVER LOADIiNG");
   driver_log("[DRIVER] NVME DRIVER LOADING");
   printf("starting ramdisk\n");
   sleep_ms(2000);
-  printf("/dev/nvme0n1p\n");
-  
+  printf("[BOOT] /dev/nvme0n1p\n");
 }
