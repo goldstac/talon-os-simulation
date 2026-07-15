05c52595a577844e455fbaa2830cffc0a6866b80
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-16 03:40:16 +0400

    Update nvram.cpp

--- nvram.cpp
+++ nvram.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-void nvram(){
-        bool secure_boot = false;
-        char boot_entrie[] = "efi/boot/grub.efi";
-        // add like xmp stuff
-        
+void nvram() {
+  bool secure_boot = false;
+  char boot_entrie[] = "efi/boot/grub.efi";
+  bool tpm_2_on = true;
+  // add like xmp stuff
 }
