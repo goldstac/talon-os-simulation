6248060149dd4665136975f6f32cf63e6d33876f
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-15 21:22:33 +0400

    Update gpuinfo.cpp

--- filesystem/proc/gpuinfo.cpp
+++ filesystem/proc/gpuinfo.cpp
@@ -16,7 +16,7 @@ int main(){
     std::cout << "--------------------------------";
     std::cout << "Name              |" << gpu_name << "\n";
     std::cout << "VRAM              |" << vram << "\n";
-    std::cout << "CUDA Cores        |" << cuda_cores << "";
+    std::cout << "CUDA Cores        |" << cuda_cores << "\n";
     std::cout << "RAM Type          |" << memory_type << "";
     std::cout << "Architecture      |" << architecture << "";
     std::cout << "Interface         |" << interface << "";
