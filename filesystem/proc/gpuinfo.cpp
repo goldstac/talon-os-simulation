647b81193b213a9a3f0fb7c0a8a113d40d3388fa
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-15 21:23:24 +0400

    Update gpuinfo.cpp

--- filesystem/proc/gpuinfo.cpp
+++ filesystem/proc/gpuinfo.cpp
@@ -13,7 +13,7 @@ int main(){
     std::string bandwith = "1.79 TB/S";
     std::cout << "--------------------------------";
     std::cout << "          GPU INFO";
-    std::cout << "--------------------------------";
+    std::cout << "----------------------------------------";
     std::cout << "Name              |" << gpu_name << "\n";
     std::cout << "VRAM              |" << vram << "\n";
     std::cout << "CUDA Cores        |" << cuda_cores << "\n";
