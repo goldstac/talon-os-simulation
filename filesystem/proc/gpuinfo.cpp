4cc8653d22ae4b6429b03cf41f0a6d488eeec419
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-15 21:17:24 +0400

    Update gpuinfo.cpp

--- filesystem/proc/gpuinfo.cpp
+++ filesystem/proc/gpuinfo.cpp
@@ -4,7 +4,22 @@
 int main(){
 
     std::string gpu_name = "Nvidia GeForce RTX 5090";
-    std::string board_partner = "Asus";
     std::string vram = "32 GB";
-    std::cout << "--------------------------------"
+    std::string memory_type = "GDDR7";
+    int cuda_cores = 21760;
+    std::string architecture = "Blackwell";
+    std::string interface = "PCIE 5.0 X16 Slot";
+    std::string memory_bus = "512-Bit";
+    std::string bandwith = "1.79 TB/S";
+    std::cout << "--------------------------------";
+    std::cout << "          GPU INFO";
+    std::cout << "--------------------------------";
+    std::cout << "Name              |" << <<;
+    std::cout << "VRAM              |" << << "";
+    std::cout << "CUDA Cores        |" << << "";
+    std::cout << "RAM Type          |" << << "";
+    std::cout << "Architecture      |" << << "";
+    std::cout << "Interface         |" << << "";
+    std::cout << "Memory Bus        |" << << "";
+    std::cout << "Memory Bandwith   |" 
 }
