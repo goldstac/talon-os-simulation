3eebeb6407972b65da1646b2d6568767e0cfd300
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-15 20:38:50 +0400

    Update gpuinfo.cpp

--- filesystem/proc/gpuinfo.cpp
+++ filesystem/proc/gpuinfo.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include "gpuinfo.h"
-int main() {
-    s
+int main(){
+
+    std::string gpu_name = "Nvidia GeForce RTX 5090";
+    std::string board_partner = "Asus";
+    std::string vram = "32 GB";
+    std::cout << "--------------------------------"
 }
