b15fd578ad1738ef234032ecbec7a78a73765061
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-19 05:01:44 +0400

    Update bootloader.cpp

--- bootloader.cpp
+++ bootloader.cpp
@@ -3,4 +3,7 @@ int main() {
   std::string bootloader_name = "Talon Bootloader\n";
   std::string bootloader_option;
   std::cout << bootloader_name;
+  std::cout << "Boot Option : [1] Talon Linux\n";
+  std::cout << "Enter Boot Option : ";
+  std::cin >> bootloader_option;
 }
