7536978201928ae45f7e3ac6d6566983e9171e8a
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-27 23:32:45 +0400

    .

--- driver_registry.h
+++ driver_registry.h
@@ -1,4 +1,4 @@
-#ifndef driver_registery
+#ifndef driver_registry
 #define driver_registry
 #include <string>
 void register_device(std::string device_name);
--- quick.sh
+++ quick.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 // this is for developer testing not for actual use
-g++ -o main.exe main.cpp driver_registry.cpp kernel/kernel.c filesystem/proc/cpuinfo.cpp filesystem/proc/gpuinfo.cpp uefi.cpp logo.cpp bootloader.cpp make_system_dir.cpp kernel/initramfs.cpp
+g++ -std=c++17 -o main.exe main.cpp driver_registry.cpp kernel/kernel.c filesystem/proc/cpuinfo.cpp filesystem/proc/gpuinfo.cpp uefi.cpp logo.cpp bootloader.cpp make_system_dir.cpp kernel/initramfs.cpp
 ./main.exe
