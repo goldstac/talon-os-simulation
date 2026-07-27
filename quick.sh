b1d8324cf13afa2f954b2a48fdb5e95bf41b70ff
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-27 23:48:01 +0400

    yes

--- quick.sh
+++ quick.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 // this is for developer testing not for actual use
-g++ -std=c++17 -o main.exe main.cpp driver_registry.cpp kernel/kernel.c filesystem/proc/cpuinfo.cpp filesystem/proc/gpuinfo.cpp uefi.cpp logo.cpp bootloader.cpp make_system_dir.cpp kernel/initramfs.cpp
+g++ -o main.exe -std=c++17 main.cpp driver_registry.cpp kernel/kernel.c filesystem/proc/cpuinfo.cpp filesystem/proc/gpuinfo.cpp uefi.cpp logo.cpp bootloader.cpp make_system_dir.cpp kernel/initramfs.cpp
 ./main.exe
